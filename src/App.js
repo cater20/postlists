3d097ac9edc972d12e42c1f4ced6b8eaa24c4dd3
cater <103543555+cater20@users.noreply.github.com>
2022-07-31 17:58:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import './App.css';
 function App() {
  const [posts,setPosts]=useState([])
  const [isDisplayed,setIsDisplayed]=useState(false)
+const [isDarkMode, setIsDarkMode] = useState(false);
 
+ function handleDarkModeClick() {
+   setIsDarkMode((isDarkMode) => !isDarkMode);
+ }
+ 
   useEffect(()=>{
     fetch('http://localhost:3000/posts')
     .then((responce)=>responce.json())
@@ -22,14 +27,18 @@ setIsDisplayed(isDisplayed=>isDisplayed=!isDisplayed)
   setPosts([...posts,newPost])
 }
   return (
-    <div className="App">
+    <div className="App"><div>
+      <Header>
+      <h2>Posters</h2>
+        <button onClick={handleDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+      </Header>
+   
       <div className="sidebar">
       <h1>Learn react</h1>
       <button onClick={handleClick} >{isDisplayed? "Show ":"hide "}new post form</button>
         {isDisplayed ? <NewPostForm onSubmission={handleNewPost}
-        
-        
-        
         /> : null}
       </div>
       <PostContainer posts={posts}/>
